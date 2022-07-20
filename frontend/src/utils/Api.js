7c1901e403ce2c357f92e4d48580c1df0d40cbd0
Stanislav Sorochan <stas.sorochan@gmail.com>
2022-07-20 22:25:13 +0200

    update

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -71,7 +71,6 @@ class Api {
 export const api = new Api({
   baseUrl: "https://api.ctacleo.nomoredomains.xyz",
   headers: {
-    authorization: "787a2830-fec0-4832-b8d1-85b319400036",
     "Content-Type": "application/json",
   },
 });
