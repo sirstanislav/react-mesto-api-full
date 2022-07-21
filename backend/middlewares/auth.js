0963f9177377f6cd4f54d7a89b2ec809dd09fb57
Stanislav Sorochan <stas.sorochan@gmail.com>
2022-07-21 23:48:48 +0200

    update

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -12,6 +12,20 @@ const isAuthorized = (req, res, next) => {
 
   const token = auth.replace('Bearer ', '');
 
+  const YOUR_JWT = token; // вставьте сюда JWT, который вернул публичный сервер
+  const SECRET_KEY_DEV = 'dev-secret'; // вставьте сюда секретный ключ для разработки из кода
+  try {
+    payload = jwt.verify(YOUR_JWT, SECRET_KEY_DEV);
+
+    console.log('\x1b[31m%s\x1b[0m', 'Надо исправить. В продакшне используется тот же секретный ключ, что и в режиме разработки.');
+  } catch (err) {
+    if (err.name === 'JsonWebTokenError' && err.message === 'invalid signature') {
+      console.log('\x1b[32m%s\x1b[0m', 'Всё в порядке. Секретные ключи отличаются');
+    } else {
+      console.log('\x1b[33m%s\x1b[0m', 'Что-то не так', err);
+    }
+  }
+
   try {
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
