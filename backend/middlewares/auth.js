dfbe751b6a1dccab6ebe84e198ffd36ed423d4f1
Stanislav Sorochan <stas.sorochan@gmail.com>
2022-07-22 00:38:33 +0200

    update

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,3 +1,4 @@
+const { NODE_ENV, JWT_SECRET } = process.env;
 const jwt = require('jsonwebtoken');
 const { TrowUnauthorizedError } = require('../errors/trowUnauthorizedError');
 
@@ -12,9 +13,18 @@ const isAuthorized = (req, res, next) => {
   const token = auth.replace('Bearer ', '');
 
   try {
-    payload = jwt.verify(token, 'secret');
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
+    console.log('\x1b[31m%s\x1b[0m', `
+      Надо исправить. В продакшне используется тот же
+      секретный ключ, что и в режиме разработки.
+    `);
   } catch (err) {
-    throw new TrowUnauthorizedError('Авторизуйтесь для доступа!');
+    // throw new TrowUnauthorizedError('Авторизуйтесь для доступа!');
+    if (err.name === 'JsonWebTokenError' && err.message === 'invalid signature') {
+      console.log('\x1b[32m%s\x1b[0m', 'Всё в порядке. Секретные ключи отличаются');
+    } else {
+      console.log('\x1b[33m%s\x1b[0m', 'Что-то не так', err);
+    }
   }
 
   req.user = payload;
