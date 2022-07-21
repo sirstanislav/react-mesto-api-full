1eb7282f74f7c87004dffe085a9c13557c3d794d
Stanislav Sorochan <stas.sorochan@gmail.com>
2022-07-21 02:15:09 +0200

    update

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -30,7 +30,7 @@ module.exports.createUser = (req, res, next) => {
     })
       .then((user) => {
         const data = {
-          id: res._id,
+          id: req.user._id,
           name: user.name,
           about: user.about,
           avatar: user.avatar,
