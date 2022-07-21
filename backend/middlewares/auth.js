9e0a7105f537bdde43ec1df981ef8343c38e3c9a
Stanislav Sorochan <stas.sorochan@gmail.com>
2022-07-21 23:47:08 +0200

    update

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -18,20 +18,6 @@ const isAuthorized = (req, res, next) => {
     throw new TrowUnauthorizedError('Авторизуйтесь для доступа!');
   }
 
-  const YOUR_JWT = token; // вставьте сюда JWT, который вернул публичный сервер
-  const SECRET_KEY_DEV = 'dev-secret'; // вставьте сюда секретный ключ для разработки из кода
-  try {
-    payload = jwt.verify(YOUR_JWT, SECRET_KEY_DEV);
-
-    console.log('\x1b[31m%s\x1b[0m', 'Надо исправить. В продакшне используется тот же секретный ключ, что и в режиме разработки.');
-  } catch (err) {
-    if (err.name === 'JsonWebTokenError' && err.message === 'invalid signature') {
-      console.log('\x1b[32m%s\x1b[0m', 'Всё в порядке. Секретные ключи отличаются');
-    } else {
-      console.log('\x1b[33m%s\x1b[0m', 'Что-то не так', err);
-    }
-  }
-
   req.user = payload;
   next();
 };
