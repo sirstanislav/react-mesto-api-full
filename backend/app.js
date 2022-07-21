00f899191be6d09b00ebab71ec2d035cb104fd4d
Stanislav Sorochan <stas.sorochan@gmail.com>
2022-07-22 00:27:10 +0200

    update

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,6 @@
 require('dotenv').config();
+
+console.log(process.env.NODE_ENV); // production
 const server = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
