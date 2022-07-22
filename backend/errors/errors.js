375990955061354c3c28687ff37f16ea6631bbcc
Stanislav Sorochan <stas.sorochan@gmail.com>
2022-07-22 12:35:43 +0200

    update

--- backend/errors/errors.js
+++ backend/errors/errors.js
@@ -1,12 +1,11 @@
-const error = (err, res, errorName, errorMessage) => {
-  if (err.name === errorName) {
-    return res.status(400).send({
-      message: errorMessage,
-    });
-  }
-  return res.status(500).send({ message: '500 — Ошибка по умолчанию' });
-};
+// const error = (err, res, errorName, errorMessage) => {
+//   if (err.name === errorName) {
+//     res.status(400).send({ message: errorMessage });
+//   } else {
+//     res.status(500).send({ message: '500 — Ошибка по умолчанию' });
+//   }
+// };
 
-module.exports = {
-  error,
-};
+// module.exports = {
+//   error,
+// };
