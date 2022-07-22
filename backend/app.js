d34c1b3f9f7c851e0a2e03de668b4f7c20c79d52
Stanislav Sorochan <stas.sorochan@gmail.com>
2022-07-22 12:30:10 +0200

    update

--- backend/app.js
+++ backend/app.js
@@ -74,7 +74,7 @@ app.use((err, req, res, next) => {
   if (err.statusCode) {
     res.status(err.statusCode).send({ message: err.message });
   } else {
-    res.status(500).send({ message: err.message });
+    res.status(500).send({ message: 'На сервере произошла ошибка' });
   }
 
   next();
