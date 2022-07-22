30058185d1bf53457500f6ba0c0359abcffe022f
Stanislav Sorochan <stas.sorochan@gmail.com>
2022-07-22 12:29:41 +0200

    update

--- backend/app.js
+++ backend/app.js
@@ -72,9 +72,9 @@ app.use(errors()); // обработчик ошибок celebrate
 // здесь обрабатываем все ошибки
 app.use((err, req, res, next) => {
   if (err.statusCode) {
-    res.send({ message: err.message });
+    res.status(err.statusCode).send({ message: err.message });
   } else {
-    res.status(500).send({ message: 'На сервере произошла ошибка' });
+    res.status(500).send({ message: err.message });
   }
 
   next();
